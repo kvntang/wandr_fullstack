b450d1ea3630add264d8ba2d6a9840df7e5c1b1a
Kevin <kevintang1509@gmail.com>
2024-10-22 21:55:02 -0400

    try2

--- client/views/CreateView.vue
+++ client/views/CreateView.vue
@@ -2,21 +2,8 @@
 import CreatePostForm from "../components/Post/CreatePostForm.vue";
 import { useUserStore } from "@/stores/user";
 import { storeToRefs } from "pinia";
-import { fetchy } from "@/utils/fetchy";
 
 const { isLoggedIn } = storeToRefs(useUserStore());
-
-async function getPosts(author?: string) {
-  let query: Record<string, string> = author !== undefined ? { author } : {};
-  let postResults;
-  try {
-    postResults = await fetchy("/api/posts", "GET", { query });
-  } catch (_) {
-    return;
-  }
-  searchAuthor.value = author ? author : "";
-  posts.value = postResults;
-}
 </script>
 
 <template>
@@ -24,7 +11,7 @@ async function getPosts(author?: string) {
     <h1>Create a post:</h1>
     <section v-if="isLoggedIn">
       <h2>Create a post:</h2>
-      <CreatePostForm @refreshPosts="getPosts" />
+      <CreatePostForm />
     </section>
   </main>
 </template>
