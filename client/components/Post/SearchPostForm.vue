7de3d71e2682ba1f483d4cbc92b5a0de344818c2
Kevin <kevintang1509@gmail.com>
2024-10-24 22:03:23 -0400

    Update SearchPostForm.vue

--- client/components/Post/SearchPostForm.vue
+++ client/components/Post/SearchPostForm.vue
@@ -1,18 +1,17 @@
 <script setup lang="ts">
-import { ref } from "vue";
-
-const author = ref("");
-const emit = defineEmits(["getPostsByAuthor"]);
+// const author = ref("");
+// const emit = defineEmits(["getPostsByAuthor"]);
 </script>
 
 <template>
-  <form @submit.prevent="emit('getPostsByAuthor', author)" class="pure-form">
+  <h1>Hi there</h1>
+  <!-- <form @submit.prevent="emit('getPostsByAuthor', author)" class="pure-form">
     <fieldset>
       <legend>Search by Author</legend>
       <input id="author" type="text" v-model="author" placeholder="Username" />
       <button type="submit" class="pure-button pure-button-primary">Search</button>
     </fieldset>
-  </form>
+  </form> -->
 </template>
 
 <style scoped>
